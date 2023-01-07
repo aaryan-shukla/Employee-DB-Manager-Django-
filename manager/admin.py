77da5bb13a94fe90fd8076ab990d2d809144a6d6
aaryan-shukla <76279578+aaryan-shukla@users.noreply.github.com>
2023-01-07 19:13:11 +0530

    Add files via upload

---
+++ manager/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Department,Role,Employee
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Department)
+# user = Aaryan
+# password = shukla1702
